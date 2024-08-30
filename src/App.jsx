3b296d6c2ca8541a4dcbdd7fd4469880e39d2ec3
dinesh048 <dinesh.regmi048@gmail.com>
2024-08-30 08:21:41 +0545

    remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import PrintHello from "./components/PrintHello";
 import Student from "./components/Student";
-const students = [
-  { name: "Hari", age: 20, bio: "I am a computer science student with major focus on web development" },
-  { name: "Joe", age: 22, bio: "I am a business student with major focus on marketing" },
-  { name: "Ram", age: 21, bio: "I am a computer science student with major focus on data science" },
-];
+
 
 function App() {
   return (
