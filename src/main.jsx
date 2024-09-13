c304224a79e57d719d3d5a501bf62a3520edafb4
dinesh048 <dinesh.regmi048@gmail.com>
2024-09-13 06:38:37 +0545

    chore: update react layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,33 +3,40 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { 
    createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
+  RouterProvider
 } from 'react-router-dom'
 import Student from './components/Student.jsx'
+import StudentList from './components/StudentList.jsx'
+import DefaultLayout from './Layout/Defaultlayout.jsx'
+
+
+
 
 const Router = createBrowserRouter([
-  {
-    path: '/about',
-    element: <h1>About Us</h1>
-  },
-  {
-    path: '/',
-    element: <h1>Home  </h1>
-  },
-  {
-    path: '/students',
-    element: <Student />
-  }
+    {
+       path: '/',
+       element: <DefaultLayout />,
+       children: [
+        {
+          path: '/about',
+          element: <h1>About Us</h1>
+        },
+        {
+          path: '/',
+          element: <h1>Home  </h1>
+        },
+        {
+          path: '/students',
+          element: <StudentList />
+        }
+
+       ]
+      }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <a  className="btn btn-outline-primary m-3" href="/">Home</a>
-    <a className="btn btn-outline-primary m-3" href="/about">About</a>
-    <a  className="btn btn-outline-primary m-3" href="/students">Students</a>
     <RouterProvider router={Router} />
   </StrictMode>,
 )
